7f37c5727c5887d486921e2484d4aeea0a211c13
Teeradon-Hen <72624679+Teeradon-Hen@users.noreply.github.com>
2020-11-04 18:15:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -12,100 +12,116 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {fname: 'Thycha a3ตัว',lname : 'เด็กเปรต'});
+    res.render('index', {fname: 'aaa',lname : 'aaa'});
 });
 
 app.listen(PORT, function () {
     console.log(`Listening on ${PORT}`)
 });
 
+
+var resultFromRequest ; 
+var stuID; 
+var pass;
 app.get('/api', function (req, res) {
-    const queryParams = req.query;
-    console.log('param[1]:' + queryParams['user']);
-    res.send(queryParams);
-});
+        const queryParams = req.query;
+        stuID = queryParams['stuID'];
+        pass = queryParams['pass'];
+        var options = {
+            'method': 'POST',
+            'hostname': 'restapi.tu.ac.th',
+            'path': '/api/v1/auth/Ad/verify',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Application-Key': '{TUc038d055adcc6fcc2717c458eb10b427da293391b5a1f633a87a9699e57f919c97572cb365827845a28540a289978a04}'
+            }
+        };
+        console.log(stuID+pass)
+        var req = https.request(options, function (res) {
+            var chunks = [];
+        
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+        
+            res.on("end", function (chunk) {
+                resultFromRequest = Buffer.concat(chunks);
+                console.log(resultFromRequest.toString());
+            });
+        
+            res.on("error", function (error) {
+                console.error(error);
+            });
+        });
+        var postData =  "{\n\t\"UserName\":\"{"+stuID+"}\",\n\t\"PassWord\":\"{"+pass+"}\"\n}";
+        postData.
+        req.write(postData);
+        req.end();
+        res.send(resultFromRequest);
+}); 
+var https = require('https');
 
 
 
-
-//var options = {
-//    'method': 'POST',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v1/auth/Ad/verify',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//};
-
-//var req = https.request(options, function (res) {
-//    var chunks = [];
-
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    });
-
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    });
-
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//});
-
-
-//var options = {
-//    'method': 'GET',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v2/std/fac/all',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//};
-
-//var req = https.request(options, function (res) {
-//    var chunks = [];
-
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    });
-
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    });
-
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//});
-
-//req.end();
-
-
-const options = {
-    hostname: 'jsonplaceholder.typicode.com',
-    path: '/todos/1',
-    method: 'GET',
-    'headers': {
-        'Content-Type': 'application/json',
+var options = {
+        'method': 'GET',
+        'hostname': 'restapi.tu.ac.th',
+        'path': '/api/v2/std/fac/all',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Application-Key': '{TUc038d055adcc6fcc2717c458eb10b427da293391b5a1f633a87a9699e57f919c97572cb365827845a28540a289978a04}'
     }
 };
-const req = http.request(options, response => {
-    response.setEncoding('utf8');
-    response.on('data', chunk => {
-        console.log('data:' + chunk);
+    
+    var req = https.request(options, function (res) {
+        var chunks = [];
+    
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+    
+        res.on("end", function (chunk) {
+            var resultFromRequest = Buffer.concat(chunks);
+            console.log(resultFromRequest.toString());
+        });
+    
+        res.on("error", function (error) {
+            console.error(error);
+        });
     });
-
-    response.on('end', () => {
-        console.log('end of GET request');
+    console.log("55555555");
+    req.end();
+    app.get('/', function (req, res) {
+        obj = JSON.parse(resultFromRequest);
+        obj.displayname_en
+        res.render('index1', {fname: 'aaa',lname : 'aaa'});
     });
-});
-
-req.on('error', e => {
-    console.log('Problem with request:', e.message);
-});
-req.end();
+    
+    /*
+    const options1 = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: '/todos/1',
+        method: 'GET',
+        'headers': {
+            'Content-Type': 'application/json',
+        }
+    };
+    const req1 = http.request(options, response => {
+        response.setEncoding('utf8');
+        response.on('data', chunk => {
+            console.log('data:' + chunk);
+        });
+    
+        response.on('end', () => {
+            console.log('end of GET request');
+        });
+    });
+    
+    req1.on('error', e => {
+        console.log('Problem with request:', e.message);
+    });
+    req1.end();
+    
+     );
+*/
+  //  res.send(queryParams);
